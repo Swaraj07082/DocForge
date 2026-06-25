4e14145e53a61dbc098cdce5d566c1da3d2af61a
Swaraj Mali <swarajmali072004@gmail.com>
2026-06-26 02:41:59 +0530

    pydantic field

--- agents/refactoring_agent.py
+++ agents/refactoring_agent.py
@@ -2,11 +2,22 @@ from groq import Groq
 import json
 from repo_analyser import analyse_symbol
 from pydantic import BaseModel
-from typing import List
+from typing import List , Literal
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Finding(BaseModel):
-    type: str
+    type: Literal[ "long_function",
+    "duplicate_code",
+    "dead_code",
+    "high_complexity",
+    "large_class",
+    "poor_naming",
+    "single_responsibility_violation",
+    "tight_coupling"]
     severity: str
     confidence: float
     title: str
@@ -15,7 +26,9 @@ class Finding(BaseModel):
 
 
 class RefactorAgentResponse(BaseModel):
-    agent: str
+    agent: Literal["refactoring"] = "refactoring" 
+    # means assigning default value
+    # only this  -> agent: Literal["refactoring_agent"] , means only refactoring_agent is allowed as value but no default value set
     symbol: str
     findings: List[Finding]
 
@@ -62,7 +75,7 @@ class RefactoringAgent:
         
         # print(prompt)
 
-        client = Groq()
+        client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
         chat_completion = client.chat.completions.create(
             model = self.model,
@@ -83,15 +96,15 @@ class RefactoringAgent:
             "schema": RefactorAgentResponse.model_json_schema()
         }
     }
-    
-   
         )
+
+        return chat_completion.choices[0].message.content
         
 
 
 
 
-obj = RefactoringAgent()
-analysis : list = obj.refactor_code("app.py")
-print(analysis)
+obj = RefactoringAgent("openai/gpt-oss-20b")
+response : list = obj.refactor_code("app.py")
+print(response)
 
