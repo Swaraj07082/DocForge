ad553603bd118795c7ea1069c2489ccab4656013
Swaraj Mali <swarajmali072004@gmail.com>
2026-06-25 19:29:24 +0530

    added pydantic schema for llm response

--- agents/refactoring_agent.py
+++ agents/refactoring_agent.py
@@ -1,27 +1,28 @@
 from groq import Groq
 import json
+from repo_analyser import analyse_symbol
+from pydantic import BaseModel
+from typing import List
+
+
+class Finding(BaseModel):
+    type: str
+    severity: str
+    confidence: float
+    title: str
+    reasoning: str
+    recommendation: str
+
+
+class RefactorAgentResponse(BaseModel):
+    agent: str
+    symbol: str
+    findings: List[Finding]
 
 class RefactoringAgent:
-    
-#     def __init__(self , model):
 
-#         self.model = model
-#         client = Groq()
-
-#         chat_completion = client.chat.completions.create(
-#             messages=[
-#         {
-#             "role": "system",
-#             "content": "You are a helpful assistant."
-#         },
-#         {
-#             "role": "user",
-#             "content": "Explain the importance of fast language models",
-#         }
-#     ],
-#     model = self.model
-# )
-
+    def __init__(self , model):
+        self.model = model
 
     def refactor_code(self , file_name):
         
@@ -31,14 +32,66 @@ class RefactoringAgent:
         # print(symbol_index)
 
         functions : list = []
+        # getting all the functions from the symbol index that are in the file_name
 
         for symbol in symbol_index:
             if symbol_index[symbol][0] == f"{file_name}":
                 functions.append(symbol)
 
+        analysis = []
+        for fn in functions:
+            analysis.append(analyse_symbol(fn))
+
+        
+
+        prompt = f"""
+        You are a senior software engineer.
+        
+        Review this code:
+        
+        {analysis}
+        
+        Find:
+        - Long functions
+        - Duplication
+        - SRP violations
+        - Readability issues
+        
+        Return findings.
+"""
+        
+        # print(prompt)
+
+        client = Groq()
+
+        chat_completion = client.chat.completions.create(
+            model = self.model,
+            messages=[
+        {
+            "role": "system",
+            "content": "You are a helpful assistant."
+        },
+        {
+            "role": "user",
+            "content": prompt,
+        }
+    ],
+     response_format={
+        "type": "json_schema",
+        "json_schema": {
+            "name": "sql_query_generation",
+            "schema": RefactorAgentResponse.model_json_schema()
+        }
+    }
+    
+   
+        )
+        
+
+
 
 
 obj = RefactoringAgent()
-obj.refactor_code("app.py")
-
+analysis : list = obj.refactor_code("app.py")
+print(analysis)
 
